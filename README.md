6dd4e8e546ee2e4b88be27dc89f5bc09ee676614
yak9909 <Mayu8680@yahoo.co.jp>
2020-11-23 01:48:34 +0900

    引数をdictからlistにした

--- README.md
+++ README.md
@@ -10,40 +10,34 @@
  先頭と末尾の文字も指定できます。
 
 # 使い方
- ```python
- # 最大値
- num_max = 100
- # 現在値
- num_now = 35
- 
- bar_nums = {
-     "max": num_max
-     "now": num_now
- }
- 
- # バーの長さ
- bar_range = 10
- 
- # バーの文字
- """
- valid -> 残り
- invalid -> 空
- 
- 1: 先頭
- 2: 中間
- 3: 末尾
- """
- bar_format = {
-     "1_valid": "◀",
-     "1_invalid": "◁",
-     "2_valid": "■",
-     "2_invalid": "□",
-     "3_valid": "▶",
-     "3_invalid": "▷"
- }
+```python
+# 最大値
+num_max = 100
+# 現在値
+num_now = 35
 
- bar = get_bar(bar_nums, bar_range, bar_format)
- print(f'{"".join(bar["valid"])}{"".join(bar["invalid"])}')
- print(bar["percent"])
- get_bar()
- ```
+bar_nums = [
+ num_max,
+ num_now
+]
+
+# バーの長さ
+bar_range = 10
+
+# バーの文字
+"""
+リストの要素数
+0, 2, 4 -> 残り
+1, 3, 5 -> 空
+"""
+bar_format = [
+    "◀", "◁", # 先頭
+    "■", "□", # 中間
+    "▶", "▷" # 末尾
+ ]
+
+bar = get_bar(bar_nums, bar_range, bar_format)
+print(f'{"".join(bar["valid"])}{"".join(bar["invalid"])}')
+print(bar["percent"])
+get_bar()
+```
--- generate_bar.py
+++ generate_bar.py
@@ -2,10 +2,10 @@ import math
 import tools
 
 
-def get_bar(nums: dict, bar_range, bar_format):
-    valid = math.ceil(nums["now"] / (nums["max"] / bar_range))
+def get_bar(nums: list, bar_range: int, bar_format: list):
+    valid = math.ceil(nums[0] / (nums[1] / bar_range))
     invalid = bar_range - valid
-    percent = math.ceil((nums["now"] / nums["max"]) * 100)
+    percent = math.ceil((nums[0] / nums[1]) * 100)
 
     if invalid > bar_range:
         invalid = bar_range
@@ -13,16 +13,16 @@ def get_bar(nums: dict, bar_range, bar_format):
     start_times = 0
     end_times = valid
     valid_bar = tools.add_str(bar_range, start_times, end_times,
-                              bar_format["1_valid"],
-                              bar_format["2_valid"],
-                              bar_format["3_valid"])
+                              bar_format[0],
+                              bar_format[2],
+                              bar_format[4])
 
     start_times = bar_range - invalid
     end_times = bar_range
     invalid_bar = tools.add_str(bar_range, start_times, end_times,
-                                bar_format["1_invalid"],
-                                bar_format["2_invalid"],
-                                bar_format["3_invalid"])
+                                bar_format[1],
+                                bar_format[3],
+                                bar_format[5])
     return {
         'valid': valid_bar,
         'invalid': invalid_bar,
@@ -33,10 +33,10 @@ def get_bar(nums: dict, bar_range, bar_format):
 if __name__ == '__main__':
     bar_num_max = tools.input_int("最大値: ")
     bar_num_now = tools.input_int("現在値: ")
-    bar_nums = {
-        "max": bar_num_max,
-        "now": bar_num_now
-    }
+    bar_nums = [
+        bar_num_now,
+        bar_num_max
+    ]
 
     bar_range = 10
     while True:
@@ -48,14 +48,14 @@ if __name__ == '__main__':
         except ValueError:
             continue
 
-    bar_format = {
-        "1_valid": "◀",
-        "1_invalid": "◁",
-        "2_valid": "■",
-        "2_invalid": "□",
-        "3_valid": "▶",
-        "3_invalid": "▷"
-    }
+    bar_format = [
+        "◀",
+        "◁",
+        "■",
+        "□",
+        "▶",
+        "▷"
+    ]
 
     bar = get_bar(bar_nums, bar_range, bar_format)
     print(f'{"".join(bar["valid"])}{"".join(bar["invalid"])}')
